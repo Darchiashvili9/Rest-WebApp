0ef8f4ede9d4c82231c089ac509c741252edb55d
axalisamsa1827 <gdarchiashvili@hotmail.com>
2023-04-25 15:58:03 +0400

    RESTful web service with controler
    Product - Supplier - Category database

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -14,16 +14,45 @@ namespace WebApp.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Product> GetProducts()
+        public IAsyncEnumerable<Product> GetProducts()
         {
-            return _dataContext.Products;
+            return _dataContext.Products.AsAsyncEnumerable();
         }
 
         [HttpGet("{id}")]
-        public Product? GetProduct([FromServices] ILogger<ProductsController> logger)
+        public async Task<IActionResult> GetProduct(long id)
         {
-            logger.LogDebug("getProduct Action Invoked");
-            return _dataContext.Products.FirstOrDefault();
+            Product? p = await _dataContext.Products.FindAsync(id);
+
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return Ok(p);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct([FromBody] ProductBindingTarget target)
+        {
+            var p = target.ToProduct();
+
+            await _dataContext.Products.AddAsync(target.ToProduct());
+            await _dataContext.SaveChangesAsync();
+            return Ok(p);
+        }
+
+        [HttpPut]
+        public async Task UpdateProduct([FromBody] Product prod)
+        {
+            _dataContext.Update(prod);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task DeleteProduct(long id)
+        {
+            _dataContext.Products.Remove(new Product() { ProductId = id });
+            await _dataContext.SaveChangesAsync();
         }
 
     }
