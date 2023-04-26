d0bf10e6fbe5d70db82f1d5a8a7a9d3b72ae8a65
axalisamsa1827 <gdarchiashvili@hotmail.com>
2023-04-26 16:46:06 +0400

    API Analizer

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace WebApp.Controllers
 {
@@ -21,6 +22,9 @@ namespace WebApp.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
         public async Task<IActionResult> GetProduct(long id)
         {
             Product? p = await _dataContext.Products.FindAsync(id);
